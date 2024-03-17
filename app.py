8e88986cd1916813f86a589ce715ba2256567e68
skawashin1122 <941574_teacher@g.miyazaki-c.ed.jp>
2024-03-17 09:55:11 +0900

    first commit

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    books = [
+        {
+        'title': 'キセキの大逆転',
+        'price': 1200,
+        'arrival_day': '2024年3月14日'
+        },
+        {
+        'title': '常識を疑え',
+        'price': 1420,
+        'arrival_day': '2024年3月15日'
+        }
+    ]
+    return render_template('index.html', books=books)
+
+@app.route('/form')
+def form():
+    return render_template('form.html')
+
+if __name__=='__main__':
+    app.run('0.0.0.0', 5500, debug=True)
